5347ae5dc2fbf8d00facb86c6abd930fcb440c36
Xinyang <xinyzhang9@gmail.com>
2016-09-07 12:14:20 -0700

    chat

---
+++ chat/server.js
@@ -0,0 +1,10 @@
+var app = require('express')();
+var http = require('http').Server(app);
+
+app.get('/',function(req,res){
+	res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(8888,function(){
+	console.log('listening on *:8888');
+});
