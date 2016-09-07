b748b8e06d4a330efef9836be5de856d5b7d2a08
Xinyang <xinyzhang9@gmail.com>
2016-09-07 15:36:07 -0700

    notify join and leave

--- chat/server.js
+++ chat/server.js
@@ -1,10 +1,39 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-app.get('/',function(req,res){
-	res.sendFile(__dirname + '/index.html');
+app.get('/', function(req, res){
+  res.sendfile('index.html');
 });
 
-http.listen(8888,function(){
-	console.log('listening on *:8888');
+var nicknames = {};
+
+io.on('connection', function(socket){
+  socket.on('disconnect',function(){
+  	var msg = ''
+  	if((nicknames[socket.id])){
+  		msg = nicknames[socket.id] + ' left the chat.';
+  	}else{
+  		msg = 'Someone left the chat.';
+  	}
+  	
+  	io.emit('left room',msg)
+  })
+  socket.on('chat message', function(msg){
+    io.emit('chat message',msg);
+  });
+  socket.on('enter room',function(name){
+  	nicknames[socket.id] = name;
+  	var msg = '';
+  	if(name){
+  		msg = name + ' has joined the chat.';
+  	}else{
+  		msg = "Someone has joined the chat."
+  	}	
+  	io.emit('enter room',msg)
+  })
+});
+
+http.listen(3000, function(){
+  console.log('listening on port:3000');
 });
